5ae99c20362a4694fd91b5140c52dd62d78a4a14
bigzhu <vermiliondun@gmail.com>
2018-09-27 18:59:21 +0800

    调整样式

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,107 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TabbedAppBarSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        // brightness: Brightness.light,
+        primaryColor: Colors.white, //Changing this will change the color of the TabBar
+        // accentColor: Colors.cyan[600],
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: DefaultTabController(
+            length: choices.length,
+            child: Scaffold(
+                appBar: AppBar(
+                    // backgroundColor: Colors.white,
+                    // title: const Text('Follow Center'),
+                    title: Row(children: [
+                      Image.asset('assets/icon.png', fit: BoxFit.cover, alignment: Alignment.centerLeft)
+                    ]),
+                    bottom: TabBar(
+                        isScrollable: false,
+                        tabs: choices.map((Choice choice) {
+                          return Tab(
+                              text: choice.title,
+                              // icon: Icon(choice.icon),
+                          );
+                        }).toList(),
+                    ),
+                ),
+                body: TabBarView(
+                    children: choices.map((Choice choice) {
+                      return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ChoiceCard(choice: choice),
+                      );
+                    }).toList(),
+                ),
+                ),
+                ),
+                );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Choice {
+  const Choice({ this.title, this.icon });
   final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  final IconData icon;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const List<Choice> choices = <Choice>[
+  Choice(title: '首页'),
+  Choice(title: '寻他'),
+];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({ Key key, this.choice }) : super(key: key);
+
+  final Choice choice;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+        color: Colors.white,
+        child: Center(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(choice.icon, size: 128.0, color: textStyle.color),
+                  Text(choice.title, style: textStyle),
+                ],
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+void main() {
+  runApp(TabbedAppBarSample());
+}
+
+/*
+   Sample Catalog
+
+   Title: Tabbed AppBar
+
+   Summary: An AppBar with a TabBar for navigating pages just below it.
+
+   Description:
+   A TabBar can be used to navigate among the pages displayed in a TabBarView.
+   Although a TabBar is an ordinary widget that can appear anywhere, it's most often
+   included in the application's AppBar.
+
+   Classes: AppBar, DefaultTabController, TabBar, Scaffold, TabBarView
+
+   Sample: TabbedAppBarSample
+
+   See also:
+   - The "Components-Tabs" section of the material design specification:
+   <https://material.io/go/design-tabs>
+   */
