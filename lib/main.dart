233aed6cea800efa0df0e6b904420c499a26d3fb
bigzhu <vermiliondun@gmail.com>
2018-09-28 15:07:34 +0800

    可以渲染显示 html

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_html_view/flutter_html_view.dart';
 
 class TabbedAppBarSample extends StatelessWidget {
   @override
@@ -17,7 +18,6 @@ class TabbedAppBarSample extends StatelessWidget {
             length: choices.length,
             child: Scaffold(
                 appBar: AppBar(
-                    // title: const Text('Follow Center'),
                     title: Row(children: [
                       Image.asset('assets/logo.png', fit: BoxFit.cover, alignment: Alignment.centerLeft), 
                       Text('Follow Center'),
@@ -61,6 +61,7 @@ class ChoiceCard extends StatelessWidget {
   const ChoiceCard({ Key key, this.choice }) : super(key: key);
 
   final Choice choice;
+  String html = '<h1>This is heading 1</h1> <h2>This is heading 2</h2><h3>This is heading 3</h3><h4>This is heading 4</h4><h5>This is heading 5</h5><h6>This is heading 6</h6><p><img alt="Test Image" src="https://i.ytimg.com/vi/RHLknisJ-Sg/maxresdefault.jpg" /></p>';
 
   @override
   Widget build(BuildContext context) {
@@ -72,6 +73,7 @@ class ChoiceCard extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
+                  HtmlView(data: html),
                   Icon(choice.icon, size: 128.0, color: textStyle.color),
                   Text(choice.title, style: textStyle),
                 ],
